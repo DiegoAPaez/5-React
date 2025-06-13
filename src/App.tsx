1c2e5bab80d9d96ff3c6093c021d1a91ec1fcf27
DiegoAPaez <diego.p31@hotmail.com>
2025-06-13 12:43:55 +0200

    Implemented Downloads section containing Card elements, styles applied, pending ladder view on Cards.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './components/organisms/Header/Header.tsx';
 import Footer from './components/organisms/Footer/Footer.tsx';
 import Hero from './components/organisms/Hero/Hero.tsx';
 import Features from './components/organisms/Features.tsx';
+import Downloads from './components/organisms/Downloads/Downloads.tsx';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Header />
             <Hero />
             <Features />
+            <Downloads />
             <Footer />
         </>
     );
--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -13,10 +13,10 @@ export default function Card({
     iconAlt: string;
 }) {
     return (
-        <div className="shadow-lg flex flex-col justify-center items-center py-10 rounded-3xl">
+        <div className="shadow-lg flex flex-col justify-center items-center p-10 rounded-3xl">
             <Icon styles="my-15" path={iconPath} iconAlt={iconAlt} />
-            <h4>{title}</h4>
-            <p>Minimum version {description}</p>
+            <h4 className="text-4xl font-bold my-5">{title}</h4>
+            <p className="text-(--text-gray) text-3xl mb-5">Minimum version {description}</p>
             <Button
                 styles="font-bold py-5 px-15 border-2 border-transparent rounded-lg bg-(--main-color) text-(--bg-gray) hover:bg-white hover:text-(--main-color) hover:border-(--main-color) cursor-pointer"
                 content="Add & Install Extension"
