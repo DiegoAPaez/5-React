cc659bd53a59fc40b62acdcfed94ad9632df3107
DiegoAPaez <diego.p31@hotmail.com>
2025-06-13 12:22:01 +0200

    Data updated with browsers information

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,9 @@
 import bookingFeature from '../assets/images/illustration-features-tab-1.svg';
 import searchFeature from '../assets/images/illustration-features-tab-2.svg';
 import shareFeature from '../assets/images/illustration-features-tab-3.svg';
+import chromeBrowser from '../assets/images/logo-chrome.svg';
+import firefoxBrowser from '../assets/images/logo-firefox.svg';
+import operaBrowser from '../assets/images/logo-opera.svg';
 
 export interface Feature {
     title: string;
@@ -30,5 +33,29 @@ export const FEATURES: Record<string, Feature> = {
         image: shareFeature,
         description:
             'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+    },
+};
+
+export interface Browser {
+    name: string;
+    image: string;
+    version: number;
+}
+
+export const BROWSERS: Record<string, Browser> = {
+    Chrome: {
+        name: 'Chrome',
+        image: chromeBrowser,
+        version: 62,
+    },
+    Firefox: {
+        name: 'Firefox',
+        image: firefoxBrowser,
+        version: 55,
+    },
+    Opera: {
+        name: 'Opera',
+        image: operaBrowser,
+        version: 46,
     },
 };
