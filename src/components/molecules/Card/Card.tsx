723f9c090e4737a6f8780c057f05009282c87018
DiegoAPaez <diego.p31@hotmail.com>
2025-06-14 17:43:35 +0200

    Implemented screen size detection to adjust elements styles.

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -1,19 +1,41 @@
+import { useEffect, useState } from 'react';
 import Button from '../../atoms/Button/Button.tsx';
 import Icon from '../../atoms/Icon/Icon.tsx';
 
 export default function Card({
+    index,
     title,
     description,
     iconPath,
     iconAlt,
 }: {
+    index?: number;
     title: string;
-    description: string;
+    description: number;
     iconPath: string;
     iconAlt: string;
 }) {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkScreen = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+        checkScreen();
+        window.addEventListener('resize', checkScreen);
+        return () => {
+            window.removeEventListener('resize', checkScreen);
+        };
+    }, []);
+
+    const marginTop = index ? index * 40 : 0;
+    const style = isMobile ? undefined : { marginTop };
+
     return (
-        <div className="shadow-lg flex flex-col justify-center items-center p-10 rounded-3xl">
+        <div
+            style={style}
+            className="shadow-lg flex flex-col justify-center items-center p-10 rounded-3xl"
+        >
             <Icon styles="my-15" path={iconPath} iconAlt={iconAlt} />
             <h4 className="text-4xl font-bold my-5">{title}</h4>
             <p className="text-(--text-gray) text-3xl mb-5">Minimum version {description}</p>
