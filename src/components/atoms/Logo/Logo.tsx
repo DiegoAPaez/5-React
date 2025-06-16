9e65edb85b582e9c230c8d2540eff768ee1fd5c0
DiegoAPaez <diego.p31@hotmail.com>
2025-06-16 10:23:01 +0200

    Added new optional 'styles' prop

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -4,13 +4,15 @@ export default function Logo({
     source,
     alt,
     type,
+    styles,
 }: {
     source: string;
     alt: string;
     type: string;
+    styles?: string;
 }): JSX.Element {
     return (
-        <picture>
+        <picture className={styles}>
             <source srcSet={source} type={type} />
             <img src={source} alt={alt} />
         </picture>
